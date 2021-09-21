a910a87f1e28ce0fe0cb688f0eae0d3455a39f37
rdagher <roudy.dagher@inria.fr>
2021-09-21 11:06:24 +0200

    Added support for influx format logg

--- dist/desire_coap_server/event_logger/file_logger.py
+++ dist/desire_coap_server/event_logger/file_logger.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass, field
+from json import encoder
 from typing import Any, IO
 import os
 from urllib.parse import urlparse
 import json
+
+from influxdb_client.client.write import point as influx_point
 
 from .common import DesireEvent, EventLogger
 from desire_coap.payloads import Base64Encoder
@@ -12,6 +15,7 @@ from desire_coap.payloads import Base64Encoder
 class FileEventLogger(EventLogger):
     path: str = field(init=False)
     handle: IO[Any] = field(init=False, default=None)
+    format:str = field(default='json')
 
     def __post_init__(self):
         # check uri file
@@ -21,6 +25,8 @@ class FileEventLogger(EventLogger):
         if os.access(self.path, os.R_OK):
             assert os.access(
                 self.path, os.W_OK), f'File {self.path} is not accessible for writing'
+        ## check output format
+        assert self.format == 'json' or self.format == 'influx', f"invalid format {self.format} must be json|influx"
 
     def connect(self, mode="w") -> None:
         self.handle = open(self.path, mode)
@@ -31,8 +37,25 @@ class FileEventLogger(EventLogger):
     def is_connected(self) -> bool:
         return not self.handle.closed
 
-    def log(self, data: DesireEvent) -> None:
+    def log(self, data: DesireEvent, format='json') -> None:
         # write event in json format
-        line = json.dumps(data.to_influx_dict(),cls=Base64Encoder)
-        self.handle.write(line + '\n')
+        lines = []
+        if self.format == 'influx':
+            points = data.to_influx_dict()
+            # convert bytearrays to base64 trick
+            points = json.loads(json.dumps(points, cls=Base64Encoder))
+            assert isinstance(points, list) or isinstance(points, dict)
+            if isinstance(points, list):
+                for p in points:
+                    point = influx_point.Point.from_dict(p)
+                    lines.append(point.to_line_protocol())
+                # process a list of measurments (batch)
+            else:
+                point = influx_point.Point.from_dict(points)
+                lines.append(point.to_line_protocol())
+        else:
+            lines.append(json.dumps(data.to_influx_dict(),cls=Base64Encoder))
+        
+        for line in lines:
+            self.handle.write(line + '\n')
 
--- dist/desire_coap_server/event_logger/tests/test_file_logger.py
+++ dist/desire_coap_server/event_logger/tests/test_file_logger.py
@@ -1,3 +1,4 @@
+import pytest
 import json
 
 from dacite import data
@@ -73,6 +74,31 @@ def test_file_event_ertl():
         logged_ert_evt = ErtlEvent.from_influx_dict(json.loads(line))
         handle.close()
         assert logged_ert_evt == ertl_evt
+    finally:
+        # delete log_file
+        os.remove(flog.path)
+
+@pytest.mark.skip("WIP: Visual test only")
+def test_file_event_ertl_influx():
+    try:
+        flog = FileEventLogger('file:./log-ertl-tmp.influx', format='influx')
+        flog.connect()
+        assert flog.is_connected()
+
+        data = '{"epoch":332,"pets":[{"pet":{"etl":"vwqMHjrpYru3s3BhZJqNpdv7yVTcukv9j22PNHEzSkI=","rtl":"UFGTQCsxu3f7l2QsKwpnimSW1vfuBBp3C2C8rdAmg14=","exposure":780,"req_count":432,"avg_d_cm":151}},{"pet":{"etl":"2IDGdmnLl2JDBRxfjVsC5MMqMdA1lGjlqzUjnlmS9Ew=","rtl":"EDfFx+xAXrsAaIJaNbUgdVFf0WTktZIiyJwzhF7dqBQ=","exposure":640,"req_count":323,"avg_d_cm":71}}]}'
+        ertl =  ErtlPayload.from_json_str(data)
+        ertl_evt = ErtlEvent(node_id='dw1234', payload=ertl)
+        flog.log(ertl_evt)
+        flog.disconnect()
+        assert not flog.is_connected()
+        print('lines=\n',open(flog.path).read())
+
+        # TODO parse log file
+        #handle = open(flog.path, 'r')
+        #line = handle.readline()
+        #logged_ert_evt = ErtlEvent.from_influx_dict(json.loads(line))
+        #handle.close()
+        #assert logged_ert_evt == ertl_evt
     finally:
         # delete log_file
         os.remove(flog.path)
